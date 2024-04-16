d7fadc5af68415c4cffce5caffa5c5a33e4082c3
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-04-16 11:40:23 +0800

    updated

--- src/components/transactions/obo/payment/Info.tsx
+++ src/components/transactions/obo/payment/Info.tsx
@@ -64,13 +64,13 @@ const PaymentInformation = () => {
         <Textbox
           label="Payer Name"
           labelStyle="!p-1"
-          className="w-full !p-4 text-[40px] text-start border-b-2 border-black rounded-none !h-[70px]"
+          className="w-full !p-4 text-[40px] text-start border-b-2 border-black rounded-none !h-[50px]"
           ref={payerNameInput}
         />
         <Textbox
           label="Payer Address"
           labelStyle="!p-1"
-          className="w-full !p-4 text-[40px] text-start border-b-2 border-black rounded-none !h-[70px]"
+          className="w-full !p-4 text-[40px] text-start border-b-2 border-black rounded-none !h-[50px]"
           ref={payerAddressInput}
         />
       </div>
